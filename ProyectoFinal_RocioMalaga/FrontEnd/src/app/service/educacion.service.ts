fa7eb84b15ea806d6263b3547255e830590a7543
RocioMalaga <romalaga17@gmail.com>
2022-08-29 22:52:52 -0300

    .

--- ProyectoFinal_RocioMalaga/FrontEnd/src/app/service/educacion.service.ts
+++ ProyectoFinal_RocioMalaga/FrontEnd/src/app/service/educacion.service.ts
@@ -6,7 +6,7 @@ import { Educacion } from '../model/educacion'
   providedIn: 'root'
 })
 export class EducacionService {
-URL = 'http://localhost:8080/educacion/';
+URL = 'https://rociomalaga-fortfolio.herokuapp.com/educacion/';
 
 constructor(private httpClient : HttpClient) { }
 
--- ProyectoFinal_RocioMalaga/FrontEnd/src/app/service/persona.service.ts
+++ ProyectoFinal_RocioMalaga/FrontEnd/src/app/service/persona.service.ts
@@ -8,7 +8,7 @@ import { persona } from '../model/persona.module';
   providedIn: 'root'
 })
 export class PersonaService {
-  URL = 'http://localhost:8080/personas/';
+  URL = 'https://rociomalaga-fortfolio.herokuapp.com/personas/';
 
   constructor(private http: HttpClient) { }
 
